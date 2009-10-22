dd3c82fa6e98cb5551ee2b62b05fc465359c8c33
Developer <developer@developer.agoramedia.com>
2009-10-22 10:49:09 -0400

    Adding ability to pass a count to ping until

--- httping.rb
+++ httping.rb
@@ -17,7 +17,7 @@ class Float
     case self
       when 0..1 then "#{(self * 1000).floor} msecs"
       when 1..2 then "1 sec"
-      else "#{self.floor} secs"
+      else "#{floor} secs"
     end
   end
 end
@@ -25,9 +25,9 @@ end
 class Fixnum
   def to_friendly_size
     case self
-      when 0..1024 then "#{self} bytes"
       when 1024..1048576 then "#{self / 1024} kb"
       when 1048576..1073741824 then "#{self / 1048576} mb"
+      else "#{self} bytes"
     end
   end
 end
@@ -35,6 +35,10 @@ end
 class HTTPing
   include Net
   include URI
+
+  def count=(count)
+    @count = count.to_i
+  end
     
   def uri=(uri)
     @uri = URI.parse(uri)
@@ -48,12 +52,14 @@ class HTTPing
   end
 
   def run
+    @pings_sent = 0 if @count
     @ping_results = []
     
-    trap("INT") { on_exit }
-    while(true) do 
+    trap("INT") { results }
+    loop do 
       ping
-    end
+      check_and_increment_counter if @count
+    end    
   end
 
   def ping
@@ -64,8 +70,14 @@ class HTTPing
     puts "#{data.length.to_friendly_size} from #{@uri}: code=#{response.code} msg=#{response.message} time=#{difference.to_friendly_time}"
   end
   
-  def on_exit
-    puts "\n\n--- #{@uri} httping statistics ---"
+  def check_and_increment_counter
+    @pings_sent += 1
+    results if @count == @pings_sent
+  end
+  
+  def results
+    puts
+    puts "--- #{@uri} httping.rb statistics ---"
     puts "#{@ping_results.size} GETs transmitted"
     puts "round-trip min/avg/max = #{@ping_results.min.to_friendly_time}/#{@ping_results.mean.to_friendly_time}/#{@ping_results.max.to_friendly_time}"
     exit
@@ -75,8 +87,8 @@ end
 if ARGV[0]
   httping = HTTPing.new
   httping.uri = ARGV[0]
+  httping.count = ARGV[1] if ARGV[1]
   httping.run
 else
-  puts "Usage: httping.rb <url>"
-  exit  
+  puts "Usage: httping.rb <url> [count]"
 end
